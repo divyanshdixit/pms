e07c96bab6949b84b90d4cd35a945440d1cfe608
divyanshdixit <divyanshdixit96@gmail.com>
2020-08-25 00:18:53 +0530

    chat feature add url -> /chat

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ var userAPI = require('./apis/user');
 var dotenv = require('dotenv').config();
 
 var app = express();
+var http = require('http').Server(app)
+var io = require('socket.io')(http);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -51,6 +53,37 @@ app.use('/api', addCatAPI )
 app.use('/api', productAPI )
 app.use('/userapi', userAPI )
 
+app.get('/chat', function(req, res){
+  res.render('getUsername', {title:'Chat Web App', projectName:'Chat Feature- Password Management System'})
+})
+
+var users = [];
+var connectedUser = 0;
+
+io.on('connection', function(socket){
+  console.log('User is connected!');
+  connectedUser++;
+
+  socket.on('setusername', function(data){
+    if(users.indexOf(data) > -1){
+      socket.emit('userExists', data + ' Username is already taken. Please choose other.')
+    }else{
+      users.push(data);
+      socket.emit('userSet', {username:data, usernumber:users.length})
+    }
+  })
+
+  socket.on('msg', function(data){
+    io.sockets.emit('newmsg', data)
+  })
+
+  socket.on('disconnect', function(){
+    console.log('User is disconnected!');
+    connectedUser--;
+
+  })
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -66,5 +99,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+http.listen(5000, function(){
+  console.log('Server is running on posrt 5000');
+})
 
 module.exports = app;
